b878ffc22d4cdafeb84d6b015d092ab299b83950
Efimchik S <pabotanet@gmail.com>
2021-11-24 13:53:12 +0300

    delete popups --test

--- src/js/main.js
+++ src/js/main.js
@@ -6,5 +6,4 @@ let btnMobMenu = document.querySelector('.nav-list');
 @@include('./partial/init-header.js')
 @@include('./partial/init-accardion.js')
 @@include('./partial/init-mobMenu.js')
-@@include('./partial/init-popups.js')
 @@include('./partial/init-animate.js')
